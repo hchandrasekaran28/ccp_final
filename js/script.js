f47e408d1fb11e9447cfa15ef115905245807eea
hchandrasekaran28 <113296651+hchandrasekaran28@users.noreply.github.com>
2024-04-04 18:34:56 -0400

    changes

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".icon").addEventListener("click", function() {
+    document.querySelector("nav").classList.toggle("hidden")
+    document.querySelector("main").classList.toggle("bigger-main")
+})
